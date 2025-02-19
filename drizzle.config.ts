bd58ca88c567174545f6e05da8f6689f943aa95f
Havyn Liew Xiang Rong <liew321@hotmail.com>
2025-02-19 09:33:34 +0800

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env' });
+
+export default defineConfig({
+	schema: './src/lib/db/schema.ts',
+	out: './supabase/migrations',
+	dialect: 'postgresql',
+	dbCredentials: {
+		host: '',
+		port: 54321,
+		user: '',
+		password: '',
+		url: process.env.DATABASE_URL!
+	}
+});
